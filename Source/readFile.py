3f60f559d2973015972a65a929163081e730b3c0
akeryilmaz <akerylm@gmail.com>
2018-12-24 21:31:34 +0300

    readFiles

---
+++ Source/readFile.py
@@ -0,0 +1,55 @@
+import os
+
+def readFile(path):
+    '''reads a file of given path(string), returns the values in a list
+    CAUTION!!! It maps year, loudness and tempo to 0-1 range'''
+    songFile = open(path,"r")
+    result = []
+    for line in songFile:
+        if line.split(':')[0] == "release_date_precision":
+            continue
+        elif line.split(':')[0] == "release_date":
+            result.append((float(line.split(':')[1][:-1].split("-")[0])-1900)/119)
+            continue
+        elif line.split(':')[0] == "loudness":
+            result.append((float(line.split(':')[1][:-1])+60)/60)
+            continue
+        elif line.split(':')[0] == "tempo":
+            result.append((float(line.split(':')[1][:-1]))/212.349)
+            continue
+        result.append(float(line.split(':')[1][:-1]))
+    return result
+        
+def readPlaylistDirectory(path):
+    '''reads files in the folder of given path(string), returns the values in a list'''
+    result = []
+    for root, subDirectories, files in os.walk(path):
+        for songFile in files:
+            result.append(readFile(path + "/" +  songFile))
+    return result
+
+
+def readGenreDirectory(path):
+    result = []
+    for root, subDirectories, files in os.walk(path):
+        for subDirectory in subDirectories:
+            result.append(readPlaylistDirectory(path + "/" + subDirectory))
+    return result
+
+if __name__ == "__main__":
+    '''Find the minimum and maximum year of the songs'''
+    minYear = float(9999999)
+    maxYear = float(0)
+    allFeatures = []
+    for root, subDirectories, files in os.walk(os.path.abspath("/home/akeryilmaz/Desktop/Ceng499/project/Dataset")):
+        for subDirectory in subDirectories:
+            allFeatures.append(readGenreDirectory("/home/akeryilmaz/Desktop/Ceng499/project/Dataset/" + subDirectory))
+    for genre in allFeatures:
+        for playlist in genre:
+            for song in playlist:
+                year = song[0]
+                if year < minYear:
+                    minYear = year
+                if year > maxYear:
+                    maxYear = year
+    print minYear, maxYear
